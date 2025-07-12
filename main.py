1e5ca27d1ceeabcd49be7d55c5bff08746ff0c80
matt-m-111 <mmarks111@hotmail.com>
2025-07-12 11:04:52 -0400

    complted word count

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
+    
+def word_count(book_path):
+    count = len(get_book_text(book_path).split())
+    return count
+   
+#commeted out for assignment
+#def main():
+ #   print(get_book_text("books/frankenstein.txt"))
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
-    
+    word_total = word_count("books/frankenstein.txt")
+    print(f"{word_total} words found in the document")  
+
 main()
