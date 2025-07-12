e7131952f7613be1fa0eef63eb65df02b81d5041
matt-m-111 <mmarks111@hotmail.com>
2025-07-12 15:44:35 -0400

    project done

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import sys
 from stats import get_book_text, word_count, char_count, char_list_pair, sort_list
+
    
-book_path = "books/frankenstein.txt"
+
+
+def arg_check():
+    
+    if len(sys.argv) == 2:
+        location_check()
+        main()
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+def location_check():
+    try:
+        get_book_text(sys.argv[1])
+    except Exception as e:
+        print("Please enter a valid book location")
+        sys.exit(1)
+    
 
 def main():
+    book_path = sys.argv[1]
     print("============ BOOKBOT ============")
     print(f'Analyzing book found at {book_path}...')
     print("----------- Word Count ----------")
@@ -15,4 +35,4 @@ def main():
     
 
  
-main()
+arg_check()
