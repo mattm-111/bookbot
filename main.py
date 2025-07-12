2fc7ab67e1195f0e00a0caba01d19d25cd9b07a3
matt-m-111 <mmarks111@hotmail.com>
2025-07-12 11:42:35 -0400

    completed char count

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from stats import get_book_text, word_count
+from stats import get_book_text, word_count, char_count
    
-#commeted out for assignment
-#def main():
- #   print(get_book_text("books/frankenstein.txt"))
+
 
 def main():
     word_total = word_count("books/frankenstein.txt")
-    print(f"{word_total} words found in the document")  
+    print(f"{word_total} words found in the document")
+    print(char_count("books/frankenstein.txt"))  
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,19 @@ def get_book_text(book_path):
 def word_count(book_path):
     count = len(get_book_text(book_path).split())
     return count
+
+def char_count(book_path):
+    book_text = get_book_text(book_path).lower()
+    char_list = {}
+    for c in book_text:
+        if c in char_list:
+            char_list[c] += 1
+        else:
+            char_list[c] = 1
+    return char_list
+
+
+    
+
+
+
